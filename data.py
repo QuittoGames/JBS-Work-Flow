be732f5df0d9a0f0467efeee5a6b1c924af39abb
Gustavo Moreira Quitto <gustavoquitto-ieg@instituto.ad>
2025-03-19 07:01:45 -0300

    3.1v IA_Local

--- data.py
+++ data.py
@@ -12,7 +12,7 @@ class data:
     mes = datetime.now().month
     day = datetime.now().day
     alert_pid : int = 0
-    version:str = "3.0v"
+    version:str = "3.1v"
     date = [(7, 50),(8, 0),(8, 50),(9, 0),(9, 50),(10, 0),(11, 20),(11, 30),(12, 20),(12, 30),(13, 20),(13, 30),(0,0)] 
     OS_client = platform.system()
     Tasks_to_do = []
