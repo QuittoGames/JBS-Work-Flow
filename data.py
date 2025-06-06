0bb867f5759e57d8dfdb3a6924fe61ba35984482
Quitto <brunamoreiraquitto@gmail.com>
2025-06-06 12:33:14 -0300

    4.4v K2025_06_H1_10relise Bug Fix

--- data.py
+++ data.py
@@ -7,7 +7,7 @@ import subprocess
 class data:
     modules_local = ["ToDo","IA_Ollama"]
     modules = ["requests","winotify","Wingets"]
-    Debug = True
+    Debug = False
     name = "JBS Work Flow"
     script_auto_gui:bool = True # Por Padrao vem desabilitada
     wingets_active:bool = False
